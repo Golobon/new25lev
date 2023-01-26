4fd99968e245a3dde0e6eb5d5ec74851184db7c8
Golobon <golobon@ya.ru>
2023-01-26 17:30:57 +0300

    Commit

--- src/Test/Sverka.java
+++ src/Test/Sverka.java
@@ -12,59 +12,51 @@ public class Sverka {
 
         List<Path> list1 = new ArrayList<>();
         List<Path> list2 = new ArrayList<>();
-        List<Integer> list3 = new ArrayList<>();
+        List<String> list3 = new ArrayList<>();
 
         BufferedReader bR = new BufferedReader(new InputStreamReader(System.in));
         Path path = Paths.get(bR.readLine());
         Path path2 = Paths.get(bR.readLine());
         bR.close();
         Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-                return FileVisitResult.CONTINUE;
-            }
+                return FileVisitResult.CONTINUE; }
 
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                list1.add(file);
-                return FileVisitResult.CONTINUE;
-            }
-        });
+                list1.add(file);                return FileVisitResult.CONTINUE;            }        });
 
 
         Files.walkFileTree(path2, new SimpleFileVisitor<Path>() {
-
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-                return FileVisitResult.CONTINUE;
-            }
+                return FileVisitResult.CONTINUE;            }
 
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                list2.add(file);
-                return FileVisitResult.CONTINUE;
-            }
-        });
+                list2.add(file);                return FileVisitResult.CONTINUE;            }        });
+
+
 
         for (int i = 0; i < list1.size(); i++) {
             for (int b = 0; b < list2.size(); b++) {
                 if (list1.get(i).toString().substring(6).equals(list2.get(b).toString().substring(6))) {
-                    System.out.println(list1.get(i).toString().substring(6));
-                    System.out.println(list2.get(b).toString().substring(6));
-                    System.out.println(list1.get(i).toString().substring(6).equals(list2.get(b).toString().substring(6)));
+//                    System.out.println(list1.get(i).toString().substring(6));
+//                    System.out.println(list2.get(b).toString().substring(6));
+//                    System.out.println(list1.get(i).toString().substring(6).equals(list2.get(b).toString().substring(6)));
                     list2.remove(list2.get(b)); b--;
                 }
             }
         }
 
-        System.out.println();
-        System.out.println("Проверка пути");
-        System.out.println();
+//        System.out.println();
+//        System.out.println("Проверка пути");
+//        System.out.println();
 
         for (int i = 0; i < list2.size(); i++) {
-            System.out.println(list2.get(i).getFileName().toString());
-            System.out.println(list2.get(i).getFileName().toString().startsWith("~$"));
+//            System.out.println(list2.get(i).getFileName().toString());
+//            System.out.println(list2.get(i).getFileName().toString().startsWith("~$"));
             if (list2.get(i).getFileName().toString().startsWith("~$")) {
                 list2.remove(list2.get(i)); i--;
             }
@@ -73,11 +65,16 @@ public class Sverka {
 //        E:\\3\\a
 //        E:\\3\\b
 
-        System.out.println();
-        System.out.println("Новый файлы:");
+//        System.out.println();
+        System.out.println("Новые файлы:");
         System.out.println();
 
         for (Path x : list2) {
+//            System.out.println(x);
+            list3.add(x.toString().substring(7));
+        }
+
+        for (String x : list3) {
             System.out.println(x);
         }
     }
