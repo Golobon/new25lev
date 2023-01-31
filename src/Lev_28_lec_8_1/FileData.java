8ba89339db49d42c6180f76d9a661d320846afb5
Golobon <golobon@ya.ru>
2023-01-31 23:38:35 +0300

    Commit

---
+++ src/Lev_28_lec_8_1/FileData.java
@@ -0,0 +1,11 @@
+package Lev_28_lec_8_1;
+
+public interface FileData {
+    boolean isHidden();
+
+    boolean isExecutable();
+
+    boolean isDirectory();
+
+    boolean isWritable();
+}
---
+++ src/Lev_28_lec_8_1/Solution.java
@@ -0,0 +1,21 @@
+package Lev_28_lec_8_1;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/*
+Null Object Pattern
+*/
+
+public class Solution {
+    private FileData fileData;
+
+    public Solution(String pathToFile) {
+        this.fileData =
+    }
+
+    public FileData getFileData() {
+        return fileData;
+    }
+}
