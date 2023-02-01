5e6eb01294b61462412b78f3ba3166ed4b8bcb0e
Golobon <golobon@ya.ru>
2023-02-01 22:42:17 +0300

    Commit

--- src/Lev_28_lec_8_1/Solution.java
+++ src/Lev_28_lec_8_1/Solution.java
@@ -12,9 +12,15 @@ public class Solution {
     private FileData fileData;
 
     public Solution(String pathToFile) {
-        this.fileData =
+        try {
+            this.fileData = new ConcreteFileData(Files.isHidden(Paths.get(pathToFile)),
+                    Files.isExecutable(Paths.get(pathToFile)),
+                            Files.isDirectory(Paths.get(pathToFile)),
+                                    Files.isWritable(Paths.get(pathToFile)));
+        } catch (Exception e) {
+            this.fileData = new NullFileData(e);
+            }
     }
-
     public FileData getFileData() {
         return fileData;
     }
