6fce9f2c3c5758e6de1d83f9ce6b5804d4c71f15
Golobon <golobon@ya.ru>
2023-02-06 21:27:48 +0300

    Commit

--- src/Lev_28_lec_10_1/CustomTree.java
+++ src/Lev_28_lec_10_1/CustomTree.java
@@ -2,20 +2,90 @@ package Lev_28_lec_10_1;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /*
 Построй дерево(1)
 */
 
-public class CustomTree extends AbstractList <String> implements Cloneable, Serializable{
+public class CustomTree extends AbstractList <String> implements Cloneable, Serializable {
 
-    @Override
-    public String get(int index) {
-        return null;
+    List<Entry<String>> entryList;
+
+    Entry<String> root;
+
+    public CustomTree() {
+        entryList = new ArrayList<>();
+        this.root = new Entry<>("0");
+        entryList.add(root);
     }
 
     @Override
-    public int size() {
-        return 0;
-    }
-}
+    public String get(int index) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public int size() { return entryList.size() - 1; }
+
+    @Override
+    public boolean add(String s) {
+        for (Entry<String> x : entryList) {
+
+            if (x.isAvailableToAddChildren()) {
+
+                if (x.availableToAddLeftChildren) {
+                    x.leftChild = new Entry<>(s);
+                    x.leftChild.parent = x;
+                    entryList.add(x.leftChild);
+                    x.availableToAddLeftChildren = false;
+                    return true; }
+
+                if (x.availableToAddRightChildren) {
+                    x.rightChild = new Entry<>(s);
+                    x.rightChild.parent = x;
+                    entryList.add(x.rightChild);
+                    x.availableToAddRightChildren = false;
+                    return true; } } }
+
+        return false; }
+
+    @Override
+    public void add(int index, String element) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public String set(int index, String element) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public String remove(int index) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) { throw new UnsupportedOperationException(); }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) { throw new UnsupportedOperationException(); }
+
+    public String getParent(String s) {
+        for (Entry<String> x : entryList) {
+            if (x.parent != null && x.elementName.equals(s)) return x.parent.elementName; }
+        return "It is the root directory without parent"; }
+
+    static class Entry<String> implements Serializable {
+        String elementName;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+        Entry<java.lang.String> parent;
+        Entry<java.lang.String> leftChild;
+        Entry<java.lang.String> rightChild;
+
+        public Entry(String s) {
+            elementName = s;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren () {
+            return availableToAddLeftChildren | availableToAddRightChildren; } } }
--- src/Lev_28_lec_10_1/Solution.java
+++ src/Lev_28_lec_10_1/Solution.java
@@ -5,5 +5,15 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
+
+        for (int i = 1; i < 18; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        System.out.println(list.size());
+
+        System.out.println();
+
+        System.out.println(((CustomTree) list).getParent("16"));
     }
 }
