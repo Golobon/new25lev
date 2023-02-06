1ba1261b764d5fb557953e39ad3d924052914801
Golobon <golobon@ya.ru>
2023-02-07 00:40:17 +0300

    Commit

--- src/Lev_28_lec_10_1/CustomTree.java
+++ src/Lev_28_lec_10_1/CustomTree.java
@@ -1,10 +1,7 @@
 package Lev_28_lec_10_1;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /*
 Построй дерево(1)
@@ -19,6 +16,7 @@ public class CustomTree extends AbstractList <String> implements Cloneable, Seri
     public CustomTree() {
         entryList = new ArrayList<>();
         this.root = new Entry<>("0");
+        root.isRoot = true;
         entryList.add(root);
     }
 
@@ -30,6 +28,14 @@ public class CustomTree extends AbstractList <String> implements Cloneable, Seri
 
     @Override
     public boolean add(String s) {
+
+        if (entryList.size() == 3) {
+            root.leftChild.availableToAddLeftChildren = true;
+            root.leftChild.availableToAddRightChildren = true;
+            root.rightChild.availableToAddLeftChildren = true;
+            root.rightChild.availableToAddRightChildren = true;
+        }
+
         for (Entry<String> x : entryList) {
 
             if (x.isAvailableToAddChildren()) {
@@ -56,8 +62,46 @@ public class CustomTree extends AbstractList <String> implements Cloneable, Seri
     @Override
     public String set(int index, String element) { throw new UnsupportedOperationException(); }
 
+
     @Override
-    public String remove(int index) { throw new UnsupportedOperationException(); }
+    public boolean remove(Object o) {
+        Queue<Entry<String>> fileTree = new ArrayDeque<>();
+        List<Entry<String>> e = new ArrayList<>();
+
+        if (!o.getClass().toString().equals("".getClass().toString())) { throw new UnsupportedOperationException(); }
+        else for (int i = 0; i < entryList.size(); i++) {
+            if (entryList.get(i).elementName.equals(o)) {
+                fileTree.add(entryList.get(i));
+
+                while (!fileTree.isEmpty()) {
+
+                    Entry<String> entry = fileTree.remove();
+
+                    if (entry.leftChild != null) {
+                        fileTree.add(entry.leftChild);
+                        e.add(entry.leftChild); }
+
+                    if (entry.rightChild != null) {
+                        fileTree.add(entry.rightChild);
+                        e.add(entry.rightChild); } }
+
+                entryList.remove(i); break; } }
+
+        for (int i = 0; i < entryList.size(); i++) {
+            for (int j = 0; j < e.size(); j++) {
+                if (!entryList.get(i).isRoot && entryList.get(i).equals(e.get(j))) {
+                    entryList.remove(entryList.get(i)); i--; } } }
+
+        fileTree = null;
+        e = null;
+
+        return true;
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
 
     @Override
     public boolean addAll(int index, Collection<? extends String> c) { throw new UnsupportedOperationException(); }
@@ -70,10 +114,12 @@ public class CustomTree extends AbstractList <String> implements Cloneable, Seri
 
     public String getParent(String s) {
         for (Entry<String> x : entryList) {
+            if (x.isRoot && x.elementName.equals(s)) return "It is the root directory without parent";
             if (x.parent != null && x.elementName.equals(s)) return x.parent.elementName; }
-        return "It is the root directory without parent"; }
+        return "null"; }
 
     static class Entry<String> implements Serializable {
+        boolean isRoot = false;
         String elementName;
         boolean availableToAddLeftChildren;
         boolean availableToAddRightChildren;
--- src/Lev_28_lec_10_1/Solution.java
+++ src/Lev_28_lec_10_1/Solution.java
@@ -6,14 +6,24 @@ public class Solution {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
 
-        for (int i = 1; i < 18; i++) {
+        for (int i = 1; i < 16; i++) {
             list.add(String.valueOf(i));
         }
 
-        System.out.println(list.size());
+        System.out.println("The list size is " + list.size());
+        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
 
-        System.out.println();
+        list.remove("3");
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("8"));
 
-        System.out.println(((CustomTree) list).getParent("16"));
+        list.add("16");
+        System.out.println("The expected parent is 9. The actual parent is " + ((CustomTree) list).getParent("16"));
+
+        list.remove("4");
+        list.remove("5");
+        list.remove("6");
+        System.out.println("Expected: true. Actual: " + list.add("20"));
+        System.out.println("The expected parent is 1. The actual parent is " + ((CustomTree) list).getParent("20"));
     }
 }
