35307450276017873daf6f581b49ced901f23762
Golobon <golobon@ya.ru>
2023-02-14 22:06:51 +0300

    Commit

---
+++ src/Lev_29_lec_2_1/Solution.java
@@ -0,0 +1,14 @@
+package Lev_29_lec_2_1;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/*
+Запись в существующий файл
+*/
+
+public class Solution {
+    public static void main(String... args) {
+
+    }
+}
