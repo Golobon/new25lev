a20d015672d9774d51f4c90e560dd47ca9f91173
Golobon <golobon@ya.ru>
2023-02-15 22:12:37 +0300

    Commit

--- src/Lev_29_lec_2_1/Solution.java
+++ src/Lev_29_lec_2_1/Solution.java
@@ -1,5 +1,6 @@
 package Lev_29_lec_2_1;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -8,7 +9,13 @@ import java.io.RandomAccessFile;
 */
 
 public class Solution {
-    public static void main(String... args) {
-
-    }
-}
+    public static void main(String... args) throws IOException {
+        try (RandomAccessFile rAF = new RandomAccessFile(args[0], "rw")) {
+            rAF.seek(Integer.parseInt(args[1]));
+            byte[] buff = new byte[args[2].getBytes().length];
+            rAF.read(buff, 0, args[2].getBytes().length);
+            rAF.seek(rAF.length());
+            if (new String(buff).equals(args[2])) rAF.write("true".getBytes());
+            else rAF.write("false".getBytes());
+        }
+    catch (FileNotFoundException e) { } } }
