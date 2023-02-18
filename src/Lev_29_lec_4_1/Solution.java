a47fa4b09cac11faeb8277a87f20df48c5904d61
Golobon <golobon@ya.ru>
2023-02-18 22:08:58 +0300

    Commit

--- src/Lev_29_lec_4_1/Solution.java
+++ src/Lev_29_lec_4_1/Solution.java
@@ -9,10 +9,13 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
         StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
-        System.out.println(writer.toString());
-    }
+        System.out.println(writer.toString()); }
 
     public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
-        return null;
-    }
-}
+        if (is == null) { return new StringWriter(); }
+        else { try (BufferedInputStream bIS = new BufferedInputStream(is); StringWriter sW = new StringWriter()) {
+        byte[] buff = new byte[bIS.available()];
+        int i = 0;
+        while (bIS.available() > 0) { buff[i++] = (byte) bIS.read(); }
+        sW.write(new String(buff));
+        return sW; } } } }
