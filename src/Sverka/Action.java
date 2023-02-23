a9dd5e80f40c121d4149181ac7f775344752b48f
Golobon <golobon@ya.ru>
2023-02-23 22:02:40 +0300

    Commit

--- src/Sverka/Action.java
+++ src/Sverka/Action.java
@@ -5,15 +5,14 @@ This programm made by Golobon
 All rights defends (C)(R)
  */
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class Action {
 
     private JFrame window;
 
     public Action() {
-        window = new JFrame("Сверка 2.0"); //Создаем окно и задаем ему имя
+        window = new JFrame("Сверка 1.1. Made by AVK"); //Создаем окно и задаем ему имя
         window.setSize(400,250); //Размер окна
         window.setLocationRelativeTo(null); //Чтобы окно было в центе экрана
         window.setResizable(false); //Чтобы нельзя бло изменить размер окна
