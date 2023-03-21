20414ab32a27eaec700d36ad2afab18e33959780
Golobon <golobon@ya.ru>
2023-03-22 00:22:44 +0300

    Commit

--- src/Lev_30_lec_15_1/SpaceShip.java
+++ src/Lev_30_lec_15_1/SpaceShip.java
@@ -1,17 +1,24 @@
 package Lev_30_lec_15_1;
 
 public class SpaceShip extends BaseObject {
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+    private double dx = 0;
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
     }
-
     @Override
-    public void draw(Canvas canvas) {
-
-    }
-
+    public void draw(Canvas canvas) { }
     @Override
     public void move() {
+        x = x + dx;
+        checkBorders(radius, Space.game.getWidth() - radius, radius, Space.game.getHeight() - radius);
+    }
+    public void moveLeft() { dx = -1; }
+    public void moveRight() { dx = 1; }
 
+    public void fire() {
+        Rocket r1 = new Rocket(x - 2, y);
+        Rocket r2 = new Rocket(x + 2, y);
+        Space.game.getRockets().add(r1);
+        Space.game.getRockets().add(r2);
     }
 }
