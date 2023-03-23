43395c6cc61d3345ea6f448ef449b468353efef4
Golobon <golobon@ya.ru>
2023-03-23 19:08:10 +0300

    Commit

--- src/Lev_31_lec_2_2/Solution.java
+++ src/Lev_31_lec_2_2/Solution.java
@@ -1,4 +1,16 @@
 package Lev_31_lec_2_2;
 
 public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        return n <= 0 ? 1 : n * factorial(n - 1);
+    }
 }
+
