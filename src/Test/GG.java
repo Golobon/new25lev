a0c8d6226f8e85b47481c1f5b161305519db4123
Golobon <golobon@ya.ru>
2023-04-05 00:03:18 +0300

    Commit

--- src/Test/GG.java
+++ src/Test/GG.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import org.slf4j.Logger;
 
 /*
 Измерить сколько времени занимает 10 тысяч вставок для каждого списка
@@ -21,5 +22,6 @@ public class GG {
         System.out.println((int) (Math.random() * 10));
         System.out.println((int) (Math.random() * 10));
         System.out.println((int) (Math.random() * 10));
+        Object d = new Object();
     }
 }
